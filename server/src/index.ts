31e4571427f14a5c30dd374f87f59323cb6ef9b4
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-12 21:19:25 +0700

    feat: add backend server (#50)
    * feat: initialize backend server subproject
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: add support to proxy dev app in server
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import Fastify from "fastify";
+import fastifyHttpProxy from "@fastify/http-proxy";
+import fastifyStatic from "@fastify/static";
+
+const fastify = Fastify({ logger: true });
+
+if (process.env.APP_DIST_DIR) {
+  fastify.register(fastifyStatic, {
+    root: process.env.APP_DIST_DIR,
+    prefix: "/",
+  });
+}
+
+if (process.env.APP_DEV_UPSTREAM) {
+  fastify.register(fastifyHttpProxy, {
+    upstream: process.env.APP_DEV_UPSTREAM,
+    prefix: "/",
+  });
+}
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exitCode = 1;
+}
