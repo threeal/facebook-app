ee0f6b72f928bff74cda8a30170feaabd12e9beb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-12 21:16:27 +0700

    feat: add support to proxy dev app in server
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
 import Fastify from "fastify";
+import fastifyHttpProxy from "@fastify/http-proxy";
 import fastifyStatic from "@fastify/static";
 
 const fastify = Fastify({ logger: true });
 
-fastify.register(fastifyStatic, {
-  root: process.env.APP_DIST_DIR ?? "dist",
-  prefix: "/",
-});
+if (process.env.APP_DIST_DIR) {
+  fastify.register(fastifyStatic, {
+    root: process.env.APP_DIST_DIR,
+    prefix: "/",
+  });
+}
+
+if (process.env.APP_DEV_UPSTREAM) {
+  fastify.register(fastifyHttpProxy, {
+    upstream: process.env.APP_DEV_UPSTREAM,
+    prefix: "/",
+  });
+}
 
 try {
   await fastify.listen({ port: 3000 });
