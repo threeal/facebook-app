618f1c9890a4ead2e813b16df6b859d38fe6aed5
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-14 21:18:41 +0700

    feat(app): modify enable behavior of admin page
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- app/src/admin/AdminDashboard.tsx
+++ app/src/admin/AdminDashboard.tsx
@@ -1,6 +1,73 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import Timeline from "../timeline/Timeline";
 import "./AdminDashboard.css";
 
-const AdminDashboard: React.FC = () => <h1>Welcome to the admin dashboard</h1>;
+export interface AdminDashboardProps {
+  adminSecret: string;
+  onAdminSecretInvalid: () => void;
+}
+
+const AdminDashboard: React.FC<AdminDashboardProps> = ({
+  adminSecret,
+  onAdminSecretInvalid,
+}) => {
+  const { data: isSecretVerified } = useQuery({
+    queryKey: ["admin/verify", adminSecret],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/admin/verify", {
+          method: "POST",
+          headers: { "admin-secret": adminSecret },
+          body: null,
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        return true;
+      } catch (err) {
+        console.error("Failed to verify admin:", err);
+        onAdminSecretInvalid();
+        return false;
+      }
+    },
+    initialData: false,
+  });
+
+  const [showDashboard, setShowDashboard] = useState<boolean>(true);
+
+  if (!isSecretVerified) return null;
+
+  const openDashboard = () => {
+    setShowDashboard(true);
+  };
+
+  const closeDashboard = () => {
+    setShowDashboard(false);
+  };
+
+  const exitDashboard = () => {
+    onAdminSecretInvalid();
+    window.history.replaceState({}, "", window.location.pathname);
+  };
+
+  return showDashboard ? (
+    <>
+      <h1 className="admin-title">Admin Dashboard</h1>
+      <button className="admin-button" onClick={closeDashboard}>
+        Timeline
+      </button>
+      <button className="admin-button" onClick={exitDashboard}>
+        Exit
+      </button>
+    </>
+  ) : (
+    <>
+      <h1 className="admin-title">Timeline</h1>
+      <button className="admin-button" onClick={openDashboard}>
+        Admin Dashboard
+      </button>
+      <Timeline />
+    </>
+  );
+};
 
 export default AdminDashboard;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,19 +1,44 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { StrictMode } from "react";
+import React, { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import Timeline from "./timeline/Timeline";
 import AdminDashboard from "./admin/AdminDashboard";
-import { verifyAdmin } from "./admin/utils";
 import "./index.css";
+
+const Root: React.FC = () => {
+  const [adminSecret, setAdminSecret] = useState<string | null>(() => {
+    const params = new URLSearchParams(window.location.search);
+    const adminSecret = params.get("admin-secret");
+    if (adminSecret) {
+      localStorage.setItem("adminSecret", adminSecret);
+      return adminSecret;
+    } else {
+      return localStorage.getItem("adminSecret");
+    }
+  });
+
+  const resetAdminSecret = () => {
+    setAdminSecret(null);
+    localStorage.removeItem("adminSecret");
+  };
+
+  return adminSecret ? (
+    <AdminDashboard
+      adminSecret={adminSecret}
+      onAdminSecretInvalid={resetAdminSecret}
+    />
+  ) : (
+    <Timeline />
+  );
+};
 
 const root = document.getElementById("root");
 if (root) {
   const queryClient = new QueryClient();
-  const isAdmin = await verifyAdmin();
   createRoot(root).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        {isAdmin ? <AdminDashboard /> : <Timeline />}
+        <Root />
       </QueryClientProvider>
     </StrictMode>,
   );
