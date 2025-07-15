305f62e4f4a4c45af79268917b49905896c9ef8e
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-15 22:11:24 +0700

    feat(app): add create post admin page
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ app/src/admin/posts/CreatePostPage.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import type { AdminCreatePostSchema } from "shared";
+import { useRawUsers } from "../hooks";
+
+export interface CreatePostPageProps {
+  adminSecret: string;
+  onBack: () => void;
+}
+
+const CreatePostPage: React.FC<CreatePostPageProps> = ({
+  adminSecret,
+  onBack,
+}) => {
+  const [isCreating, setIsCreating] = useState(false);
+  const [authorId, setAuthorId] = useState(-1);
+  const [timestamp, setTimestamp] = useState(-1);
+  const [caption, setCaption] = useState("");
+  const [reactions, setReactions] = useState(0);
+
+  const users = useRawUsers(adminSecret);
+  const sortedUsers = users.sort((a, b) => a.name.localeCompare(b.name));
+
+  const createPost = async () => {
+    setIsCreating(true);
+    try {
+      const data: AdminCreatePostSchema = {
+        authorId,
+        timestamp,
+        caption,
+        reactions,
+      };
+      const res = await fetch("/api/admin/posts", {
+        method: "POST",
+        headers: {
+          "admin-secret": adminSecret,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) throw new Error(res.statusText);
+    } catch (err) {
+      console.error("Failed to create post:", err);
+    } finally {
+      setIsCreating(false);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="admin-title">Create Post</h1>
+      <button className="admin-button" onClick={onBack}>
+        Back
+      </button>
+      <select
+        className="admin-input"
+        defaultValue={-1}
+        onChange={(e) => {
+          setAuthorId(parseInt(e.target.value));
+        }}
+      >
+        <option value={-1} disabled hidden>
+          Select Author
+        </option>
+        {sortedUsers.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <input
+        className="admin-input"
+        type="date"
+        onChange={(e) => {
+          if (e.target.valueAsDate) {
+            const milliseconds = e.target.valueAsDate.getTime();
+            setTimestamp(Math.floor(milliseconds / 1000));
+          } else {
+            setTimestamp(-1);
+          }
+        }}
+      />
+      <textarea
+        className="admin-input"
+        placeholder="Caption"
+        onChange={(e) => {
+          setCaption(e.target.value);
+        }}
+      ></textarea>
+      <input
+        className="admin-input"
+        type="number"
+        placeholder="Reactions"
+        inputMode="numeric"
+        onChange={(e) => {
+          setReactions(Math.floor(e.target.valueAsNumber));
+        }}
+      />
+      <button
+        className="admin-button"
+        disabled={
+          isCreating ||
+          authorId == -1 ||
+          timestamp == -1 ||
+          isNaN(reactions) ||
+          reactions < 0
+        }
+        onClick={() => void createPost()}
+      >
+        {isCreating ? "Creating Post..." : "Create Post"}
+      </button>
+    </>
+  );
+};
+
+export default CreatePostPage;
--- app/src/admin/posts/PostsPage.tsx
+++ app/src/admin/posts/PostsPage.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import { parseRawPostSchema } from "shared";
+import CreatePostPage from "./CreatePostPage";
 
-export interface PostsPageProps {
+type Page = "main" | "create";
+
+interface PostCardsProps {
   adminSecret: string;
-  onBack: () => void;
 }
 
-const PostsPage: React.FC<PostsPageProps> = ({ adminSecret, onBack }) => {
+const PostCards: React.FC<PostCardsProps> = ({ adminSecret }) => {
   const { data: posts } = useQuery({
     queryKey: ["admin/posts", adminSecret],
     queryFn: async () => {
@@ -28,10 +30,6 @@ const PostsPage: React.FC<PostsPageProps> = ({ adminSecret, onBack }) => {
 
   return (
     <>
-      <h1 className="admin-title">Posts</h1>
-      <button className="admin-button" onClick={onBack}>
-        Back
-      </button>
       {posts.map((post) => (
         <div key={post.id} className="admin-card">
           ID: {post.id}
@@ -49,6 +47,46 @@ const PostsPage: React.FC<PostsPageProps> = ({ adminSecret, onBack }) => {
       ))}
     </>
   );
+};
+
+export interface PostsPageProps {
+  adminSecret: string;
+  onBack: () => void;
+}
+
+const PostsPage: React.FC<PostsPageProps> = ({ adminSecret, onBack }) => {
+  const [page, setPage] = useState<Page>("main");
+
+  switch (page) {
+    case "main":
+      return (
+        <>
+          <h1 className="admin-title">Posts</h1>
+          <button className="admin-button" onClick={onBack}>
+            Back
+          </button>
+          <button
+            className="admin-button"
+            onClick={() => {
+              setPage("create");
+            }}
+          >
+            Create Post
+          </button>
+          <PostCards adminSecret={adminSecret} />
+        </>
+      );
+
+    case "create":
+      return (
+        <CreatePostPage
+          adminSecret={adminSecret}
+          onBack={() => {
+            setPage("main");
+          }}
+        />
+      );
+  }
 };
 
 export default PostsPage;
