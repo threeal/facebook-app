d18a09184cd12d855a2000ac4f39bef5e44fcb87
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-19 15:09:49 +0700

    feat(app): modify `UserDetailsPage` component to allow creating avatar
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- app/src/admin/users/UserDetailsPage.tsx
+++ app/src/admin/users/UserDetailsPage.tsx
@@ -2,21 +2,28 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { parseAdminUserDetails } from "shared";
 import ActionButton from "../inputs/ActionButton";
+import FileInput from "../inputs/FileInput";
 import TextInput from "../inputs/TextInput";
 import { useParseAdminSubmitUser } from "../hooks";
 import { shortenId } from "../utils";
 
-type Page = "main" | "confirm-delete" | "confirm-avatar-delete";
+type Page =
+  | "main"
+  | "confirm-delete"
+  | "create-avatar"
+  | "confirm-avatar-delete";
 
 interface UpdateUserFormProps {
   id: string;
   adminSecret: string;
+  onCreateAvatar: () => void;
   onDeleteAvatar: () => void;
 }
 
 const UpdateUserForm: React.FC<UpdateUserFormProps> = ({
   id,
   adminSecret,
+  onCreateAvatar,
   onDeleteAvatar,
 }) => {
   const { data: userDetails } = useQuery({
@@ -65,17 +72,69 @@ const UpdateUserForm: React.FC<UpdateUserFormProps> = ({
           if (!res.ok) throw new Error(res.statusText);
         }}
       />
-      {userDetails && userDetails.hasAvatar && (
-        <>
-          <label className="admin-input-label">Avatar</label>
-          <div className="admin-avatar">
-            <img src={`/static/users/avatars/${id}/40x40.webp`} />
-          </div>
-          <button className="admin-button" onClick={onDeleteAvatar}>
-            Delete User Avatar
+      {userDetails &&
+        (userDetails.hasAvatar ? (
+          <>
+            <label className="admin-input-label">Avatar</label>
+            <div className="admin-avatar">
+              <img src={`/static/users/avatars/${id}/40x40.webp`} />
+            </div>
+            <button className="admin-button" onClick={onDeleteAvatar}>
+              Delete User Avatar
+            </button>
+          </>
+        ) : (
+          <button className="admin-button" onClick={onCreateAvatar}>
+            Create User Avatar
           </button>
-        </>
-      )}
+        ))}
+    </>
+  );
+};
+
+interface CreateUserAvatarFormProps {
+  id: string;
+  adminSecret: string;
+  onCreated: () => void;
+}
+
+const CreateUserAvatarForm: React.FC<CreateUserAvatarFormProps> = ({
+  id,
+  adminSecret,
+  onCreated,
+}) => {
+  const [avatarFile, setAvatarFile] = useState<File | null>(null);
+
+  return (
+    <>
+      <FileInput
+        label="Avatar"
+        accept="image/*"
+        onFileChanged={(file) => {
+          setAvatarFile(file);
+        }}
+      />
+      <ActionButton
+        label="Create Usert Avatar"
+        processingLabel="Creating User Avatar..."
+        errorLabel="Failed to Create User Avatar"
+        disabled={!avatarFile}
+        onAction={async () => {
+          if (avatarFile) {
+            const formData = new FormData();
+            formData.append("file", avatarFile);
+
+            const res = await fetch(`/api/admin/users/${id}/avatar`, {
+              method: "POST",
+              headers: { "admin-secret": adminSecret },
+              body: formData,
+            });
+            if (!res.ok) throw new Error(res.statusText);
+
+            onCreated();
+          }
+        }}
+      />
     </>
   );
 };
@@ -104,6 +163,9 @@ const UserDetailsPage: React.FC<UserDetailsPageProps> = ({
           <UpdateUserForm
             id={id}
             adminSecret={adminSecret}
+            onCreateAvatar={() => {
+              setPage("create-avatar");
+            }}
             onDeleteAvatar={() => {
               setPage("confirm-avatar-delete");
             }}
@@ -144,6 +206,28 @@ const UserDetailsPage: React.FC<UserDetailsPageProps> = ({
           >
             Cancel
           </button>
+        </>
+      );
+
+    case "create-avatar":
+      return (
+        <>
+          <h1 className="admin-title">Create User {shortenId(id)} Avatar</h1>
+          <button
+            className="admin-button"
+            onClick={() => {
+              setPage("main");
+            }}
+          >
+            Back
+          </button>
+          <CreateUserAvatarForm
+            id={id}
+            adminSecret={adminSecret}
+            onCreated={() => {
+              setPage("main");
+            }}
+          />
         </>
       );
 
