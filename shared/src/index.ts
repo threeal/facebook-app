439a12c832106e76b3f8e612b7270874318649ad
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-19 11:24:33 +0700

    feat(shared): add `adminUserDetails` schema
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -3,5 +3,6 @@ export * from "./schemas/admin/adminPostDetails.js";
 export * from "./schemas/admin/adminPosts.js";
 export * from "./schemas/admin/adminSubmitPost.js";
 export * from "./schemas/admin/adminSubmitUser.js";
+export * from "./schemas/admin/adminUserDetails.js";
 export * from "./schemas/admin/adminUsers.js";
 export * from "./schemas/posts.js";
